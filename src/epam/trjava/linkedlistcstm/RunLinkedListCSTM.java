d299227cf758355b6e5b8c6d86da74ba0642d254
Mikhail Bahatyrou <mikhailbahatyrou@gmail.com>
2017-03-19 12:06:18 +0300

    initial

---
+++ src/epam/trjava/linkedlistcstm/RunLinkedListCSTM.java
@@ -0,0 +1,10 @@
+package epam.trjava.linkedlistcstm;
+
+public class RunLinkedListCSTM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/epam/trjava/linkedlistcstm/logic/LinkedListCstm.java
@@ -0,0 +1,5 @@
+package epam.trjava.linkedlistcstm.logic;
+
+public class LinkedListCstm {
+
+}
